1f72fd53c7febf0b824ad5e2d2886620d3cb1c76
rohitchawdhari <rohitchawdhari48@gmail.com>
2026-07-09 13:10:24 +0530

    Trigger Vercel redeploy to compile new VITE_GOOGLE_CLIENT_ID environment variable

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// Vercel build trigger for dynamic VITE_GOOGLE_CLIENT_ID environment variable
 import App from "./App";
 import "./index.css";
 
